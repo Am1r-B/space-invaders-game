2785c2287dea409d6bb2fb9f37547a82e0b3d5b9
Am1r-B <69463011+Am1r-B@users.noreply.github.com>
2023-04-30 01:05:10 +0330

    Add and draw invaders

--- app.js
+++ app.js
@@ -7,3 +7,16 @@ for (let i = 0; i < 225; i++) {
   squares.push(square);
   grid.appendChild(square);
 }
+
+const alienInvaders = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30, 31,
+  32, 33, 34, 35, 36, 37, 38, 39,
+];
+
+function draw() {
+  for (let i = 0; i < alienInvaders.length; i++) {
+    squares[alienInvaders[i]].classList.add("invader");
+  }
+}
+
+draw();
